2f8961c50102818b07ba7ad2199a2224fa5bc94c
TurtleyKam <52931649+TurtleyKam@users.noreply.github.com>
2019-07-16 13:10:53 -0600

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+def main():
+    name = input("Give a Name ")
+    place = input("Give a Place ")
+    thing = input("Give an Object ")
+    adjictive = input("Give an Adverb Ending in 'ly' ")
+    vehicle = input("Give a Vehicle ")
+    thing2 = input("Give an Object ")
+    verb = input("Give a Verb Relating to the Object ")
+    person = input("Give a Name ")
+    print("One day " + str(name) + " couldn't wait to go on a trip with " + str(person) + " they packed there things and " + str(person) + " secretly grabbed his " + str(thing) + " and headed off to " + str(place) + " they hopped on a " + str(vehicle) + ". Partway through the ride " + str(person) + " grabbed their " + str(thing2) + " and started " + str(verb) + " " + str(adjictive) + " with his " + str(thing) + " beside him. The " + str(vehicle) + " swerved off the road and crashed in a head-on collision! No one survived...")
+    
+if __name__ == "__main__":
+    main()
